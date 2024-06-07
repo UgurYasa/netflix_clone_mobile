f5b8baa1be1bec1a27354589b1260091543ee287
UgurYasa <90580549+UgurYasa@users.noreply.github.com>
2024-06-07 22:13:22 +0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <View className="flex flex-row flex-1 bg-slate-900 px-5 items-center justify-between h-96">
-      <Text className="text-white font-bold text-2xl">BulutMD</Text>
+      <Text className="text-white font-bold text-2xl">Netflix</Text>
       <TouchableOpacity
         className="px-3 py-1 bg-red-600 border-[1px] border-slate-300"
         onPress={() => {
